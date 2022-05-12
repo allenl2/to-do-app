70a4b33675fada9d1f4f8c1f4659c5744854fa99
Allen Liu <allen@Allens-MacBook-Pro.local>
2022-05-12 17:16:58 -0400

    added time creation to task model

--- app/models/task.go
+++ app/models/task.go
@@ -1,13 +1,16 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Task struct {
 	gorm.Model
-	ID       uint
-	TaskName string
-	Assignee string
-	Status   string
+	ID        uint
+	TaskName  string
+	Assignee  string
+	Status    string
+	CreatedAt time.Time
 }
