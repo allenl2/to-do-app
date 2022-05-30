764df2f9a672daa12b5a1109a83680589019a6dd
Allen Liu <58382354+allenl2@users.noreply.github.com>
2022-05-30 10:27:13 -0400

    feat: add new tasks (#24)

--- static/home.js
+++ static/home.js
@@ -1,21 +1,50 @@
 const logoutBtn = document.getElementById("logoutBtn");
+const newTaskForm = document.getElementById("newTaskForm");
+const newTask = document.getElementById("newTask");
 
 logoutBtn.addEventListener("click", handleLogout);
+newTaskForm.addEventListener("submit", addNewTask);
+
 
 function handleLogout(event) {
     event.preventDefault();
 
-    
     fetch("/api/logout", {
         method: 'POST',
     })
         .then(res => {
-            console.log(res)
+            console.log(res);
             if(res.ok) {
-                window.location.assign("/")
+                window.location.assign("/");
             }
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
         })
+}
+
+function addNewTask(event) {
+    event.preventDefault;
+
+    fetch("/api/tasks", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            taskname: newTask.value,
+            status: "inprogress"
+        })
+    })
+    .then(res => {
+        console.log(res);
+        if(res.ok) {
+            alert("added new task");
+        } else {
+            alert("unable to add new task!");
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 }
