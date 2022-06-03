dbc30e3b64fa7690587002c9958ef11959626419
allenl2 <2002allenliu@gmail.com>
2022-06-03 11:49:09 -0400

    feat: client side user validation

--- static/register.js
+++ static/register.js
@@ -6,32 +6,37 @@ form.addEventListener('submit', handleRegister);
 
 async function handleRegister(event) {
     event.preventDefault();
-    console.log("HERE")
 
-
-    await fetch("/api/user", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username.value,
-            password: password.value,
+    if (form.checkValidity() === false) {
+        event.stopPropagation();
+    }
+    //if inputs are valids, register user
+    else {
+        await fetch("/api/user", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username.value,
+                password: password.value,
+            })
         })
-    })
-        .then(res => {
-            console.log(res)
-            if(res.ok) {
-                window.location.assign("/")
-                alert("Registered successfully. Login to continue")
-            }
-            else {
+            .then(res => {
+                console.log(res)
+                if(res.ok) {
+                    window.location.assign("/")
+                    alert("Registered successfully. Login to continue")
+                }
+                else {
+                    alert("Unable to register. Please try again.")
+                }
+            })
+            .catch(err => {
+                console.log(err)
                 alert("Unable to register. Please try again.")
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            alert("Unable to register. Please try again.")
-        })
+            })
+    }
 
+    form.classList.add('was-validated');
 }
